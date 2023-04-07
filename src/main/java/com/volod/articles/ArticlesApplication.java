62ec26ca260db085b99078d880e43c6ff96208e5
Volodymyr Bolshakov <volodbol@gmail.com>
2023-04-07 14:52:36 +0300

    feat: refactor Command Line Runner bean

--- src/main/java/com/volod/articles/ArticlesApplication.java
+++ src/main/java/com/volod/articles/ArticlesApplication.java
@@ -3,17 +3,25 @@ package com.volod.articles;
 import com.volod.articles.model.ApplicationUser;
 import com.volod.articles.model.Article;
 import com.volod.articles.model.Color;
-import com.volod.articles.repository.ArticleRepository;
 import com.volod.articles.repository.ApplicationUserRepository;
+import com.volod.articles.repository.ArticleRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Articles",
+        version = "1.0"
+))
 public class ArticlesApplication {
 
     public static void main(String[] args) {
@@ -23,26 +31,37 @@ public class ArticlesApplication {
     @Bean
     public CommandLineRunner runner(
             ArticleRepository articleRepository,
-            ApplicationUserRepository applicationUserRepository) {
+            ApplicationUserRepository applicationUserRepository,
+            PasswordEncoder passwordEncoder) {
         return (String[] args) -> {
-            List<ApplicationUser> users = generateApplicationUsers(5);
+            List<ApplicationUser> users = generateApplicationUsersPlusDefault(5, passwordEncoder);
             applicationUserRepository.saveAll(users);
             users.forEach(applicationUser -> IntStream.range(0, 5)
-                            .mapToObj(number -> new Article()
-                                    .withApplicationUser(applicationUser)
-                                    .withText("random text " + number)
-                                    .withColor(Color.values()[number]))
-                            .forEach(articleRepository::save));
+                    .mapToObj(number -> new Article()
+                            .withApplicationUser(applicationUser)
+                            .withText("random text " + number)
+                            .withColor(Color.values()[number]))
+                    .forEach(articleRepository::save));
         };
     }
 
-    private static List<ApplicationUser> generateApplicationUsers(int amount) {
-        return IntStream.range(0, amount)
+    private static List<ApplicationUser> generateApplicationUsersPlusDefault(
+            int amount,
+            PasswordEncoder passwordEncoder) {
+        List<ApplicationUser> applicationUsers = IntStream.range(0, amount)
                 .mapToObj(number -> new ApplicationUser()
-                        .withName("user" + number)
+                        .withUsername("user" + number)
+                        .withPassword(passwordEncoder.encode("user" + number))
                         .withAge(number * 2 + 18)
                 )
-                .toList();
+                .collect(Collectors.toList());
+        applicationUsers.add(
+                new ApplicationUser()
+                        .withUsername("admin")
+                        .withPassword(passwordEncoder.encode("admin"))
+                        .withAge(22)
+        );
+        return applicationUsers;
     }
 
 }
