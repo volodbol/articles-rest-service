bda62dc6a83586f975b96c229d02e18281cfa94e
Volodymyr Bolshakov <volodbol@gmail.com>
2023-04-07 14:50:05 +0300

    feat: add methods to application user repository

--- src/main/java/com/volod/articles/repository/ApplicationUserRepository.java
+++ src/main/java/com/volod/articles/repository/ApplicationUserRepository.java
@@ -1,7 +1,22 @@
 package com.volod.articles.repository;
 
 import com.volod.articles.model.ApplicationUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Integer> {
+
+    Page<ApplicationUser> findAllByAgeGreaterThan(Integer age, Pageable pageable);
+
+    Page<ApplicationUser> findAll(Pageable pageable);
+
+    Boolean existsByUsername(String username);
+
+    Optional<ApplicationUser> findByUsername(String username);
+
 }
