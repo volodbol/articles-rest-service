bf951ee94a214321c539c22716eb1fddfffd5f7e
Volodymyr Bolshakov <volodbol@gmail.com>
2023-04-07 15:07:36 +0300

    feat: add auth request and response

---
+++ src/main/java/com/volod/articles/response/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.volod.articles.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+}
